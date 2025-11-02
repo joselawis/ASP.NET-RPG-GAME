463a695183f132db024c191e2741c3fccc8db7fe
joselawis <joselawis@gmail.com>
2025-11-02 14:18:29 +0100

    added Hero and Stats dtos

---
+++ Heroes.Application/DTOs/HeroDto.cs
@@ -0,0 +1,20 @@
+﻿namespace Heroes.Application.DTOs
+{
+    public sealed record HeroDto(
+     Guid Id,
+     string Name,
+     string Class,
+     int Level,
+     int Experience,
+     StatsDto Stats,
+     DateTime CreatedAt,
+     DateTime? LastBattleAt
+ );
+
+    public sealed record StatsDto(
+        int Health,
+        int Attack,
+        int Defense,
+        int Speed
+    );
+}
