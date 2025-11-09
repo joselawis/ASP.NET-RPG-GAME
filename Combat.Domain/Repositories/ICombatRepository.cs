20d4d61abf8f33a7b11e2849e21c3504ec2f4b2d
joselawis <joselawis@gmail.com>
2025-11-09 19:16:44 +0100

    combat encounter repository

---
+++ Combat.Domain/Repositories/ICombatRepository.cs
@@ -0,0 +1,16 @@
+﻿using Combat.Domain.Entities;
+using Combat.Domain.ValueObjects;
+
+namespace Combat.Domain.Repositories
+{
+    public interface ICombatRepository
+    {
+        Task<CombatEncounter?> GetByIdAsync(CombatId id, CancellationToken cancellationToken = default);
+
+        Task<List<CombatEncounter>> GetAllByHeroIdAsync(Guid heroId, CancellationToken cancellationToken = default);
+
+        Task AddAsync(CombatEncounter combat, CancellationToken cancellationToken = default);
+
+        Task UpdateAsync(CombatEncounter combat, CancellationToken cancellationToken = default);
+    }
+}
