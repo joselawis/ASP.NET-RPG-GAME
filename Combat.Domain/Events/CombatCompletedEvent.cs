d99b9cee6b3e8c7924f1716f76bf47fdfc21a1fc
joselawis <joselawis@gmail.com>
2025-11-09 19:12:42 +0100

    added events for init and complete, also copied DomainException

---
+++ Combat.Domain/Events/CombatCompletedEvent.cs
@@ -0,0 +1,15 @@
+﻿using Combat.Domain.Common;
+using Combat.Domain.ValueObjects;
+
+namespace Combat.Domain.Events
+{
+    public sealed record CombatCompletedEvent(
+        Guid CombatId,
+        Guid HeroId,
+        Guid EnemyId,
+        CombatStatus Status,
+        Guid WinnerId,
+        int ExperienceGained,
+        int TurnCount
+        ) : DomainEvent;
+}
---
+++ Combat.Domain/Events/CombatInitiatedEvent.cs
@@ -0,0 +1,6 @@
+﻿using Combat.Domain.Common;
+
+namespace Combat.Domain.Events
+{
+    public sealed record CombatInitiatedEvent(Guid CombatId, Guid HeroId, Guid EnemyId) : DomainEvent;
+}
