60e871365160ad9b652a4cd69ba390bea33a2716
joselawis <joselawis@gmail.com>
2025-11-09 19:25:05 +0100

    copied classes from heroes

---
+++ Combat.Application/Common/Interfaces/IDomainEventDispatcher.cs
@@ -0,0 +1,9 @@
+﻿using Combat.Domain.Common;
+
+namespace Combat.Application.Common.Interfaces
+{
+    public interface IDomainEventDispatcher
+    {
+        Task DispatchAsync(DomainEvent domainEvent, CancellationToken cancellationToken = default);
+    }
+}
---
+++ Combat.Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Combat.Application
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            // MediatR
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+
+            // FluentValidation
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
