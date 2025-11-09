c976d2021e5e728abb62b6e7ac1f3240430fa048
joselawis <joselawis@gmail.com>
2025-11-09 12:29:22 +0100

    mappers for hero

---
+++ Heroes.Application/Mappers/HeroMapper.cs
@@ -0,0 +1,27 @@
+﻿using Heroes.Application.DTOs;
+using Heroes.Domain.Entities;
+
+namespace Heroes.Application.Mappers
+{
+    public static class HeroMapper
+    {
+        public static HeroDto MapToDto(Hero hero)
+        {
+            return new HeroDto(
+                hero.Id.Value,
+                hero.Name.Value,
+                hero.Class.Name,
+                hero.Level.Value,
+                hero.Experience.Value,
+                new StatsDto(
+                    hero.CurrentStats.Health,
+                    hero.CurrentStats.Attack,
+                    hero.CurrentStats.Defense,
+                    hero.CurrentStats.Speed
+                ),
+                hero.CreatedAt,
+                hero.LastBattleAt
+            );
+        }
+    }
+}
