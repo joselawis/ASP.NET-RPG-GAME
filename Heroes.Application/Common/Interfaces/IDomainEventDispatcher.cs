5f652d4752975847b6d132aa3e289169be395054
joselawis <joselawis@gmail.com>
2025-11-09 12:30:40 +0100

    domainDispatcher interface and result class

---
+++ Heroes.Application/Common/Interfaces/IDomainEventDispatcher.cs
@@ -0,0 +1,9 @@
+﻿using Heroes.Domain.Common;
+
+namespace Heroes.Application.Common.Interfaces
+{
+    public interface IDomainEventDispatcher
+    {
+        Task DispatchAsync(DomainEvent domainEvent, CancellationToken cancellationToken = default);
+    }
+}
